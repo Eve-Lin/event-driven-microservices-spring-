3ad88a468da1490985d00766c4c01b86d0b68573
Eve Lin <evelin.dicheva@gmail.com>
2026-06-28 16:33:45 +0300

    setup(investigator-service)

---
+++ investigator-service/src/test/java/com/is/investigator_service/InvestigatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.is.investigator_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestigatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
